ec17f39409549caf9efa61821b2c367ce8cf70a2
sergii-onyx <twilight.cympak@gmail.com>
2022-03-26 18:54:37 -0400

    add ability to create html and refactor code

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr.forEach(profileItem => console.log(profileItem));
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
